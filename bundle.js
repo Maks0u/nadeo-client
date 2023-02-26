7c482aa838edf9ddddf807628b663e9ab19398cc
Maks0u <Maks0u@proton.me>
2023-02-26 23:28:38 +0100

    es exports

--- bundle.js
+++ bundle.js
@@ -1,7 +1,11 @@
+// import Client from './src/Client.js';
+// import { formatTime } from './src/Utils.js';
+// exports.Client = Client;
+// exports.formatTime = formatTime;
+
 import Client from './src/Client.js';
-import { formatTime } from './src/Utils.js';
-exports.Client = Client;
-exports.formatTime = formatTime;
+export default Client;
+export * from './src/Utils.js';
 
 ['UBI_USER_AGENT', 'UBI_APP_ID', 'UBI_CLIENT_ID', 'UBI_CLIENT_SECRET', 'UBI_BASE', 'NADEO_CORE_BASE', 'NADEO_LIVE_BASE'].forEach(key => {
     if (!process.env[key]) {
